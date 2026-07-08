f600a14575baada41215dfb85a6d115b3bc985a5
deboer2026 <deboer2026@users.noreply.github.com>
2026-07-08 22:18:31 +0900

    feat: jewelry_master v8 - 12営業日マップ・なぞなぞリクエスト・VIP来店・コイン/内装カスタマイズ・虹の宝石

--- src/games/JewelryMaster.jsx
+++ src/games/JewelryMaster.jsx
@@ -17,7 +17,7 @@ export default function JewelryMaster() {
         <HomeChip />
         <iframe
           ref={iframeRef}
-          src="/games/jewelry_master_v7.html"
+          src="/games/jewelry_master_v8.html"
           title="ジュエリーマスター"
           allow="autoplay; fullscreen"
           allowFullScreen
